12003ca0f12c09045f341aaa2c88f74cb53b6f9a
VidaspVii <vidasp@mail.ru>
2019-04-26 22:44:05 +0300

    Add test for dashboard

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
+describe HomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
       get :index
+
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "GET #dashboard" do
+    context 'manager' do
+      let!(:company)  { create(:company) }
+      let(:user)           { create(:user) }
+
+      before do
+        company.employers.create(user: user, post: 1)
+        login(user)
+      end
+
+      it "only user-manager returns http success" do
+        get :dashboard
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'anonym' do
+      let(:user)           { create(:user) }
+
+      before { login(user) }
+
+      it "returns http redirect" do
+        get :dashboard
+
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
 end
