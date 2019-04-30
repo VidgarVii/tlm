77e6cc1ebd827a6e99ed46b48714acdcecc9040b
Pavel <vidasp@mail.ru>
2019-04-30 13:54:31 +0300

    Update page create payment account, Add display errors

--- app/assets/javascripts/bank_accounts.js
+++ app/assets/javascripts/bank_accounts.js
@@ -2,8 +2,9 @@ document.addEventListener('turbolinks:load', () => {
 
   let result      = document.getElementById('js-search-result'),
       bikInput    = document.getElementById('bank_account_bank_id'),
+      btnFormP    = document.getElementById('js-form-btn-add-payment'),
       error       = document.getElementsByClassName('new_bank')[0],
-      resultBlock = document.getElementsByClassName('js_search_form')[0];
+      resultBlock = document.getElementsByClassName('js-search-form')[0];
 
   let addError = () => { error.classList.remove('hidden') };
   let removeError = () => { error.classList.add('hidden') };
@@ -13,6 +14,10 @@ document.addEventListener('turbolinks:load', () => {
     result.innerHTML = `<h5> ${response.bank.name} </h5> <p> ${response.bank.correspondent_account} <br> ${response.bank.legal_address.address}</p>`;
   };
 
+  let showBtnAddPayment = () => {
+    btnFormP.classList.remove('hidden')
+  };
+
   let setResult = (e) => {
     if (e.detail[0].error) {
       result.classList.add('hidden');
@@ -20,7 +25,9 @@ document.addEventListener('turbolinks:load', () => {
     } else {
       removeError();
       bikInput.value = e.detail[0].bank.id;
-      displayResult(e.detail[0]) }
+      displayResult(e.detail[0])
+      showBtnAddPayment();
+    }
   };
 
   if (resultBlock) {
